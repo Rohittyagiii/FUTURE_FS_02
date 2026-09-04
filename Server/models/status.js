426267da4696ba8138e491dd82a00623e863acf2
Rohittyagiii <tyagirohit698@gmail.com>
2026-09-04 14:24:08 +0530

    Client Management System

--- Server/models/status.js
+++ Server/models/status.js
@@ -1,1 +1,29 @@
 
+const mongoose = require("mongoose");
+
+const statusSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+
+    description: {
+      type: String,
+      trim: true,
+      default: "",
+    },
+
+    // isActive: {
+    //   type: Boolean,
+    //   default: true,
+    // },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Status", statusSchema);
